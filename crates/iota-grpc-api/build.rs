6cfcb25b899f07957554da3dea0cc5854ddfc6f3
Bing-Yang Lin <bingyang.lin@iota.org>
2025-07-24 20:21:34 +0800

    fix: remove unnecessary flag

--- crates/iota-grpc-api/build.rs
+++ crates/iota-grpc-api/build.rs
@@ -3,7 +3,6 @@
 
 fn main() {
     tonic_build::configure()
-        .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(&["proto/node.proto"], &["proto"])
         .unwrap();
 }
