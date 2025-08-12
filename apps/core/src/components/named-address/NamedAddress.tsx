bingyanglin/iota

commit ec2a82d3d9778097418a8a8a0596c506d2fbf8ec

feat(wallet,dashboard): implement a `NamedAddress` wrapper component (#7866)
## Links to any relevant issues

fixes https://github.com/iotaledger/iota/issues/7729

## How the change has been tested

- [ ] Basic tests (linting, compilation, formatting, unit/integration
tests)
- [ ] Patch-specific tests (correctness, functionality coverage)
- [ ] I have added tests that prove my fix is effective or that my
feature works
- [ ] I have checked that new and existing unit tests pass locally with
my changes

### Release Notes

- [ ] Protocol:
- [ ] Nodes (Validators and Full nodes):
- [ ] Indexer:
- [ ] JSON-RPC:
- [ ] GraphQL:
- [ ] CLI:
- [ ] Rust SDK:
- [ ] REST API:

---------

Co-authored-by: marc2332 <mespinsanz@gmail.com>
Co-authored-by: Keito <64607484+KeitoTadashi@users.noreply.github.com>
Co-authored-by: Jose Luis Torres Corbalan <jltorres@boxfish.studio>
Co-authored-by: msarcev <mario.sarcevic@iota.org>
Co-authored-by: JCNoguera <88061365+VmMad@users.noreply.github.com>

apps/core/src/components/named-address/NamedAddress.tsx
@@ -1,0 +1,55 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { Address } from '@iota/apps-ui-kit';
+import { useGetIotaName } from '../../hooks';
+import clsx from 'clsx';
+import { normalizeIotaName } from '@iota/iota-names-sdk';
+import { truncateString } from '../../utils';
+import { formatAddress } from '@iota/iota-sdk/utils';
+
+interface NamedAddressProps extends Omit<React.ComponentProps<typeof Address>, 'text'> {
+    address: string;
+    addMarginRightToCenter?: boolean;
+}
+
+export function NamedAddress({
+    address,
+    isCopyable,
+    isExternal,
+    externalLink,
+    copyText,
+    onCopySuccess,
+    onCopyError,
+    onOpen,
+    addMarginRightToCenter = false,
+}: NamedAddressProps): React.JSX.Element {
+    const { data: defaultName } = useGetIotaName(address);
+    const iotaName = defaultName && normalizeIotaName(defaultName);
+    const formattedAddress = formatAddress(address);
+
+    return (
+        <div
+            className={clsx(
+                'flex flex-col gap-y-xxs items-center',
+                addMarginRightToCenter && iotaName ? '-mr-lg' : '',
+            )}
+        >
+            {iotaName ? (
+                <span className="text-label-md dark:text-iota-neutral-92 text-iota-neutral-10 -ml-xl">
+                    {truncateString(iotaName, 12)}
+                </span>
+            ) : null}
+            <Address
+                text={formattedAddress}
+                isCopyable={isCopyable}
+                isExternal={isExternal}
+                externalLink={externalLink}
+                copyText={copyText}
+                onCopySuccess={onCopySuccess}
+                onCopyError={onCopyError}
+                onOpen={onOpen}
+            />
+        </div>
+    );
+}

apps/core/src/components/index.ts
@@ -21,3 +21,4 @@ export * from './VirtualList';
 export * from './NoData';
 export * from './Banner';
 export * from './address-alias';
+export * from './named-address';

apps/core/src/components/named-address/index.ts
@@ -1,0 +1,4 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+export * from './NamedAddress';

apps/core/src/contexts/IotaNamesClientContext.tsx
@@ -1,0 +1,53 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+'use client';
+
+import { useIotaClientContext } from '@iota/dapp-kit';
+import { IotaNamesClient } from '@iota/iota-names-sdk';
+import { getNetwork } from '@iota/iota-sdk/client';
+import React, { createContext, useContext, useMemo } from 'react';
+import { useIotaGraphQLClientContext } from './IotaGraphQLClientContext';
+
+export const IotaNamesClientProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+    const { iotaNamesClient } = useIotaNamesClient();
+
+    return (
+        <IotaNamesClientContext.Provider value={{ iotaNamesClient }}>
+            {children}
+        </IotaNamesClientContext.Provider>
+    );
+};
+
+type IotaNamesClientContextType = {
+    iotaNamesClient: IotaNamesClient | null;
+};
+
+export const IotaNamesClientContext = createContext<IotaNamesClientContextType | null>(null);
+
+export function useIotaNamesClientContext(): IotaNamesClientContextType {
+    const context = useContext(IotaNamesClientContext);
+
+    if (!context) {
+        throw new Error('useIotaNamesClientContext must be used within a IotaNamesClientProvider');
+    }
+
+    return context;
+}
+
+export function useIotaNamesClient() {
+    const ctx = useIotaClientContext();
+    const network = getNetwork(ctx.network);
+    const { iotaGraphQLClient } = useIotaGraphQLClientContext();
+
+    const iotaNamesClient = useMemo(() => {
+        if (!iotaGraphQLClient) return null;
+
+        return new IotaNamesClient({
+            graphQlClient: iotaGraphQLClient,
+            network: network.id,
+        });
+    }, [iotaGraphQLClient, network.id]);
+
+    return { iotaNamesClient };
+}

apps/core/src/contexts/index.ts
@@ -6,3 +6,4 @@ export * from './HiddenAssetsProvider';
 export * from './StardustIndexerClientContext';
 export * from './StardustIndexerClientProvider';
 export * from './IotaGraphQLClientContext';
+export * from './IotaNamesClientContext';

apps/core/src/hooks/index.ts
@@ -64,6 +64,7 @@ export * from './useMaxCommitteeSize';
 export * from './useGetNextEpochCommitteeMember';
 export * from './useAddressAliasLookup';
 export * from './useNFTMediaHeaders';
+export * from './useGetIotaName';
 
 export * from './stake';
 export * from './ui';

apps/core/src/hooks/useGetIotaName.ts
@@ -1,0 +1,19 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { useQuery } from '@tanstack/react-query';
+import { useIotaNamesClient } from '../contexts';
+
+export function useGetIotaName(address: string) {
+    const { iotaNamesClient } = useIotaNamesClient();
+
+    return useQuery({
+        queryKey: ['iota-name', 'default-name', address],
+        async queryFn() {
+            const defaultName = await iotaNamesClient?.getDefaultName(address);
+
+            return defaultName;
+        },
+        enabled: !!iotaNamesClient && address.length > 0,
+    });
+}

apps/wallet-dashboard/components/account-balance/AccountBalance.tsx
@@ -2,16 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { useCurrentAccount, useIotaClientContext } from '@iota/dapp-kit';
-import { formatAddress } from '@iota/iota-sdk/utils';
-import { useBalance, useFormatCoin, useGetFiatBalance, toast, useGetAllBalances } from '@iota/core';
 import {
-    Address,
-    Button,
-    ButtonSize,
-    ButtonType,
-    LoadingIndicator,
-    Panel,
-} from '@iota/apps-ui-kit';
+    useBalance,
+    useFormatCoin,
+    useGetFiatBalance,
+    toast,
+    useGetAllBalances,
+    NamedAddress,
+} from '@iota/core';
+import { Button, ButtonSize, ButtonType, LoadingIndicator, Panel } from '@iota/apps-ui-kit';
 import { getNetwork } from '@iota/iota-sdk/client';
 import { ReceiveFundsDialog, SendTokenDialog } from '../dialogs';
 import { useState } from 'react';
@@ -24,7 +23,6 @@ export function AccountBalance() {
     const { id: networkId, explorer } = getNetwork(network);
     const fiatBalance = useGetFiatBalance(networkId);
     const { data: coinBalance, isPending } = useBalance(address!);
-    const formattedAddress = formatAddress(address!);
     const [formatted, symbol] = useFormatCoin({ balance: coinBalance?.totalBalance });
     const [isSendTokenDialogOpen, setIsSendTokenDialogOpen] = useState(false);
     const explorerLink = `${explorer}/address/${address}`;
@@ -55,14 +53,15 @@ export function AccountBalance() {
                     <div className="flex h-full flex-col items-center justify-center gap-y-lg p-lg">
                         <div className="flex flex-col items-center gap-y-xs">
                             {address && (
-                                <div className="-mr-lg" data-full-address={address}>
-                                    <Address
-                                        text={formattedAddress}
+                                <div className="w-full" data-full-address={address}>
+                                    <NamedAddress
+                                        address={address}
                                         isCopyable
                                         copyText={address}
                                         isExternal
                                         externalLink={explorerLink}
                                         onCopySuccess={handleOnCopySuccess}
+                                        addMarginRightToCenter
                                     />
                                 </div>
                             )}
@@ -78,7 +77,7 @@ export function AccountBalance() {
                                 </span>
                             )}
                         </div>
-                        <div className="flex w-full max-w-56 gap-xs">
+                        <div className="max-w-56 flex w-full gap-xs">
                             <Button
                                 onClick={openSendTokenDialog}
                                 text="Send"

commit b5b65d4d41aa220df7b860dfa91727fe2731f01e

fix(dashboard): Center address in AccountBalance (#8174)
# Description of change

Center the address above balance

Broken in #7866

---------

Co-authored-by: marc2332 <mespinsanz@gmail.com>

apps/core/src/components/named-address/NamedAddress.tsx
@@ -30,7 +30,7 @@ export function NamedAddress({
         <div
             className={clsx(
                 'flex flex-col gap-y-xxs items-center',
-                addMarginRightToCenter && iotaName ? '-mr-lg' : '',
+                addMarginRightToCenter ? '-mr-xl' : '',
             )}
         >
             {iotaName ? (