c53ff9bd77325b25ddd50686d73ac490219670d8
Sergiu Popescu <sergiu.popescu@iota.org>
2026-02-23 16:49:23 +0100

    feat(iota-indexer): replace json rpc dry_run with grpc simulate api

--- crates/iota-indexer/src/apis/write_api.rs
+++ crates/iota-indexer/src/apis/write_api.rs
@@ -6,34 +6,56 @@ use std::sync::Arc;
 
 use async_trait::async_trait;
 use fastcrypto::encoding::Base64;
+use futures::{FutureExt, TryFutureExt};
 use iota_grpc_client::Client as GrpcClient;
+use iota_grpc_types::field::{FieldMask, FieldMaskUtil};
 use iota_json::IotaJsonValue;
 use iota_json_rpc::IotaRpcModule;
 use iota_json_rpc_api::WriteApiServer;
 use iota_json_rpc_types::{
     DevInspectArgs, DevInspectResults, DryRunTransactionBlockResponse, IotaMoveViewCallResults,
-    IotaTransactionBlockResponse, IotaTransactionBlockResponseOptions, IotaTypeTag,
-    MoveFunctionName,
+    IotaTransactionBlock, IotaTransactionBlockEffects, IotaTransactionBlockResponse,
+    IotaTransactionBlockResponseOptions, IotaTypeTag, MoveFunctionName,
 };
 use iota_open_rpc::Module;
-use iota_package_resolver::Resolver;
+use iota_package_resolver::{PackageStore, Resolver};
 use iota_protocol_config::Chain;
 use iota_transaction_builder::TransactionBuilder;
 use iota_types::{
-    base_types::IotaAddress, iota_serde::BigInt, quorum_driver_types::ExecuteTransactionRequestType,
+    base_types::IotaAddress,
+    effects::{TransactionEffects, TransactionEvents},
+    iota_serde::BigInt,
+    quorum_driver_types::ExecuteTransactionRequestType,
+    signature::GenericSignature,
+    transaction::{SenderSignedData, TransactionData},
 };
 use jsonrpsee::{RpcModule, core::RpcResult, http_client::HttpClient};
 
 use crate::{
-    apis::error::Error as ApiError, errors::IndexerError,
-    optimistic_indexing::OptimisticTransactionExecutor, read::IndexerReader,
+    apis::error::Error as ApiError,
+    errors::{IndexerError, IndexerResult},
+    ingestion::primary::prepare::InMemTxChanges,
+    metrics::IndexerMetrics,
+    models::transactions::tx_events_to_iota_tx_events,
+    optimistic_indexing::OptimisticTransactionExecutor,
+    read::IndexerReader,
     store::package_resolver::IndexerStorePackageResolver,
-    types::IotaTransactionBlockResponseWithOptions,
+    types::{IotaTransactionBlockResponseWithOptions, grpc_conversion},
 };
+
+// As an optimization, we're trying to request only the fields we actually need.
+const DRY_RUN_TRANSACTION_READ_MASK: &[&str] = &[
+    "executed_transaction.signatures.bcs",
+    "executed_transaction.effects.bcs",
+    "executed_transaction.events.events.bcs",
+    "executed_transaction.input_objects.bcs",
+    "executed_transaction.output_objects.bcs",
+    "suggested_gas_price",
+    "execution_result.execution_error.source",
+];
 
 #[derive(Clone)]
 pub struct WriteApi {
-    #[allow(dead_code)]
     fullnode_grpc_client: GrpcClient,
     transaction_builder: TransactionBuilder,
     package_resolver: Arc<Resolver<IndexerStorePackageResolver>>,
@@ -54,6 +76,103 @@ impl WriteApi {
             transaction_builder: TransactionBuilder::new(data_reader),
             package_resolver: Arc::new(Resolver::new(package_resolver)),
         }
+    }
+
+    async fn dry_run_transaction_block_impl(
+        &self,
+        tx_bytes: Base64,
+        package_resolver: &Arc<Resolver<impl PackageStore>>,
+    ) -> IndexerResult<DryRunTransactionBlockResponse> {
+        let transaction_data = bcs::from_bytes::<TransactionData>(&tx_bytes.to_vec()?)?;
+        let tx_digest = transaction_data.digest();
+
+        let readmask = FieldMask::from_paths(DRY_RUN_TRANSACTION_READ_MASK)
+            .display()
+            .to_string();
+
+        let simulate_tx_response = self
+            .fullnode_grpc_client
+            .simulate_transaction(
+                transaction_data.clone().try_into()?,
+                false,
+                false,
+                Some(readmask.as_str()),
+            )
+            .await?;
+
+        let executed_transaction = simulate_tx_response.executed_transaction()?;
+        let execution_error_source = simulate_tx_response
+            .execution_error()
+            .and_then(|e| e.source.clone());
+        let suggested_gas_price = simulate_tx_response.suggested_gas_price;
+
+        let input_objects = grpc_conversion::objects(executed_transaction.input_objects()?)?;
+        let output_objects = grpc_conversion::objects(executed_transaction.output_objects()?)?;
+
+        let objects = input_objects
+            .iter()
+            .chain(output_objects.iter())
+            .collect::<Vec<_>>();
+
+        let tx_effects: TransactionEffects =
+            executed_transaction.effects()?.effects()?.try_into()?;
+
+        let tx_signatures = executed_transaction
+            .signatures()?
+            .signatures
+            .iter()
+            .map(|s| -> IndexerResult<_> { Ok(GenericSignature::try_from(s.signature()?)?) })
+            .collect::<IndexerResult<Vec<GenericSignature>>>()?;
+
+        let sender_signed_data = SenderSignedData::new(transaction_data.clone(), tx_signatures);
+
+        let tx_events = TransactionEvents::try_from(executed_transaction.events()?.events()?)?;
+
+        let in_mem_tx_changes =
+            InMemTxChanges::new(&objects, IndexerMetrics::new(&Default::default()));
+
+        // as a minor optimization we will run concurrently the following four futures
+        let fut1 = in_mem_tx_changes
+            .get_changes(&transaction_data, &tx_effects, &tx_digest)
+            .map_ok(|(balance_changes, object_changes)| {
+                (
+                    balance_changes,
+                    object_changes
+                        .into_iter()
+                        .map(iota_json_rpc_types::ObjectChange::from)
+                        .collect::<Vec<_>>(),
+                )
+            });
+
+        let fut2 = IotaTransactionBlock::try_from_with_package_resolver(
+            sender_signed_data,
+            package_resolver,
+            tx_digest,
+        )
+        .map_err(Into::into);
+
+        // timestamp is None because it represent a checkpoint one, on a dry run
+        // operation we don't have this information.
+        let fut3 = tx_events_to_iota_tx_events(tx_events, package_resolver, tx_digest, None);
+
+        let fut4 = IotaTransactionBlockEffects::from_native_with_clever_error(
+            tx_effects.clone(),
+            package_resolver,
+        )
+        .map(Ok);
+
+        let ((balance_changes, object_changes), transaction_block, events, effects) =
+            futures::future::try_join4(fut1, fut2, fut3, fut4).await?;
+
+        Ok(DryRunTransactionBlockResponse {
+            effects,
+            events,
+            object_changes,
+            balance_changes,
+            input: transaction_block.data,
+            suggested_gas_price,
+            execution_error_source,
+        })
     }
 }
 
@@ -111,9 +230,11 @@ impl WriteApiServer for WriteApi {
 
     async fn dry_run_transaction_block(
         &self,
-        _tx_bytes: Base64,
+        tx_bytes: Base64,
     ) -> RpcResult<DryRunTransactionBlockResponse> {
-        todo!("waiting issue: #10390 and #10391 to be resolved");
+        self.dry_run_transaction_block_impl(tx_bytes, &self.package_resolver)
+            .await
+            .map_err(Into::into)
     }
 
     async fn view_function_call(
